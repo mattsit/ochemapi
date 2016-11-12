5bde215d84f3d6ad83774b8964821c8e486825c9
Matthew Sit <msit@berkeley.edu>
2016-11-12 01:24:37 -0800

    jframe starter

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Matthew on 11/12/2016.
  */
 public class Main {
-
+    public static void main(String args[]) {
+        JFrame f = new JFrame("Organic Chemistry");
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        f.setSize(400, 300);
+        JPanel p = new JPanel();
+        p.add(new JButton("test"));
+        f.getContentPane().add(p, BorderLayout.CENTER);
+        f.setVisible(true);
+    }
 }
