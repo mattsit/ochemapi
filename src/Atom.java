02e0ac095f7693edc2aea7658c70f9a18bd12740
Matthew Sit <msit@berkeley.edu>
2016-11-12 02:32:05 -0800

    atom and molecule

--- src/Atom.java
+++ src/Atom.java
@@ -1,11 +1,89 @@
+
 /**
  * Created by DebbiePao on 11/12/16.
  */
 public class Atom {
-    public int atomicnum;
+    private int atomic_num;
+    private static int next_id;
+    private int id;
 
-    public Atom(int atomicnumber) {
-        atomicnum = atomicnumber;
+    public Atom(int atomic_num) {
+        this.atomic_num = atomic_num;
+        this.id = Atom.next_id;
+        Atom.next_id++;
     }
 
+    public int getAtomNum() {
+        return this.atomic_num;
+    }
+
+    public String getAtomName() {
+        return Atom.atom_data[this.atomic_num][1];
+    }
+
+    public String getAtomSymbol() {
+        return Atom.atom_data[this.atomic_num][0];
+    }
+
+    public static boolean equals(Atom a1, Atom a2) {
+        return a1.id == a2.id
+                && a1.atomic_num == a2.atomic_num;
+    }
+
+    private static String[][] atom_data = {
+        {"Undefined", "XX"},
+        {"Hydrogen", "H"},      // 1
+        {"Helium", "He"},       // 2
+        {"Lithium", "Li"},      // 3
+        {"Beryllium", "Be"},    // 4
+        {"Boron", "B"},         // 5
+        {"Carbon", "C"},        // 6
+        {"Nitrogen", "N"},      // 7
+        {"Oxygen", "O"},        // 8
+        {"Fluorine", "F"},      // 9
+        {"Neon", "Ne"},         // 10
+        {"Sodium", "Na"},       // 11
+        {"Magnesium", "Mg"},    // 12
+        {"Aluminium", "Al"},    // 13
+        {"Silicon", "Si"},      // 14
+        {"Phosphorus", "P"},    // 15
+        {"Sulfur", "S"},        // 16
+        {"Chlorine", "Cl"},     // 17
+        {"Argon", "Ar"},        // 18
+        {"Potassium", "K"},     // 19
+        {"Calcium", "Ca"},      // 20
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Bromine", "Br"},       // 35
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Undefined", "XX"},
+        {"Iodine", "I"}           // 53
+    };
 }
--- src/Molecule.java
+++ src/Molecule.java
@@ -3,151 +3,65 @@
  */
 import java.util.*;
 public class Molecule {
-    private LinkedList<Edge>[] adjLists;
-    private int vertexCount;
-    private int startVertex;
+    private HashMap<Atom, LinkedList<Bond>> adjLists;
 
-    public Molecule(int numVertices) {
-        adjLists = new LinkedList[numVertices];
-        startVertex = 0;
-        for (int i = 0; i < numVertices; i++) {
-            adjLists[i] = new LinkedList<Edge>();
-        }
-        vertexCount = numVertices;
+    public Molecule() {
+        this.adjLists = new HashMap<>();
     }
 
-    public void setStartVertex(int a) {
-        if (a < vertexCount && a >= 0) {
-            startVertex = a;
-        } else {
-            throw new IllegalArgumentException("Cannot iteration start vertex to " + a + "." );
-        }
+    public void addAtom(Atom a) {
+        adjLists.put(a, new LinkedList<>());
     }
 
-    // Add to the graph an undirected edge from vertex v1 to vertex v2.
-    public void addUndirectedEdge(int v1, int v2) {
-        addUndirectedEdge(v1, v2, null);
-    }
-    // Add to the graph an undirected edge from vertex v1 to vertex v2,
-    // with the given edge information.
-    public void addUndirectedEdge(int v1, int v2, Object edgeInfo) {
-        adjLists[v1].addFirst(new Edge(v1, v2, edgeInfo));
-        adjLists[v2].addFirst(new Edge(v2, v1, edgeInfo));
+    public void addBond(Atom a1, Atom a2) {
+        addBond(a1, a2, 1);
     }
 
-    // Return true if there is an edge from vertex "from" to vertex "to";
+    public void addBond(Atom a1, Atom a2, int bondStrength) {
+        adjLists.get(a1).add(new Bond(a1, a2, bondStrength));
+        adjLists.get(a2).add(new Bond(a2, a1, bondStrength));
+    }
+
+    // Return true if there is an Bond from Atom "from" to Atom "to";
     // return false otherwise.
-    public boolean isAdjacent(int from, int to) {
-        for (Object b : neighbors(from)) {
-            if (b.equals(to)) {
+    public boolean isAdjacent(Atom from, Atom to) {
+        for (Object a : neighbors(from)) {
+            if (a.equals(to)) {
                 return true;
             }
         }
         return false;
     }
 
-    // Returns a list of all the neighboring  vertices 'u'
-    // such that the edge (VERTEX, 'u') exists in this graph.
-    public List neighbors(int vertex) {
-        // your code here
-        ArrayList<Integer> ans = new ArrayList<Integer>();
-        int count = 0;
-        while (count < adjLists[vertex].size()) {
-            ans.add(adjLists[vertex].get(count).getto());
-            count++;
-        }
-        return ans;
-    }
-
-    // Return the number of incoming vertices for the given vertex,
-    // i.e. the number of vertices v such that (v, vertex) is an edge.
-    public int inDegree(int vertex) {
-        int ans = 0;
-        for (int i = 0; i < vertexCount; i++) {
-            for (Object m : neighbors(i)) {
-                if (m.equals(vertex)) {
-                    ans++;
-                }
-            }
-        }
-        return ans;
-    }
-
-    // A class that iterates through the vertices of this graph, starting with a given vertex.
-    // Does not necessarily iterate through all vertices in the graph: if the iteration starts
-    // at a vertex v, and there is no path from v to a vertex w, then the iteration will not
-    // include w
-    private class DFSIterator implements Iterator<Integer> {
-
-        private Stack<Integer> fringe;
-        private HashSet<Integer> visited;
-
-        public DFSIterator(Integer start) {
-            fringe = new Stack<>();
-            visited = new HashSet<>();
-            setStartVertex(start);
-            fringe.push(start);
-            visited.add(start);
-        }
-
-        public boolean hasNext() {
-            if (!(fringe.empty())) {
-                return true;
-            }
-            return false;
-        }
-
-        public Integer next() {
-            Integer curr = fringe.pop();
-            List children = neighbors(curr);
-            for (Object i : children) {
-                if (!visited.contains(i)) {
-                    fringe.push((Integer) i);
-                    visited.add((Integer) i);
-                }
-            }
-            return curr;
-        }
-
-        public void remove() {
-            throw new UnsupportedOperationException(
-                    "vertex removal not implemented");
-        }
-
-    }
-
-    // Return the collected result of iterating through this graph's
-    // vertices as an ArrayList.
-    public ArrayList<Integer> visitAll(int startVertex) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        Iterator<Integer> iter = new DFSIterator(startVertex);
-
-        while (iter.hasNext()) {
-            result.add(iter.next());
+    public List neighbors(Atom a) {
+        ArrayList<Atom> result = new ArrayList<>();
+        for (int i = 0; i < adjLists.get(a).size(); i++) {
+            result.add(adjLists.get(a).get(i).getTo());
         }
         return result;
     }
-    private class Edge {
-        private Integer from;
-        private Integer to;
-        private Object edgeInfo;
 
-        public Edge(int from, int to, Object info) {
-            this.from = new Integer(from);
-            this.to = new Integer(to);
-            this.edgeInfo = info;
+    private class Bond {
+        private Atom from;
+        private Atom to;
+        private int bondStrength;
+
+        public Bond(Atom from, Atom to, int bondStrength) {
+            this.from = from;
+            this.to = to;
+            this.bondStrength = bondStrength;
         }
 
-        public Integer getto() {
-            return to;
+        public Atom getFrom() {
+            return this.from;
         }
 
-        public Object getinfo() {
-            return edgeInfo;
+        public Atom getTo() {
+            return this.to;
         }
 
-        public String toString() {
-            return "(" + from + "," + to + ",dist=" + edgeInfo + ")";
+        public int getBondStrength() {
+            return this.bondStrength;
         }
     }
 }
