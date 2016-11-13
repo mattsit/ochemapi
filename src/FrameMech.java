3474fe8f7714c02d13aa79b19188b6e7892b982c
Matthew Sit <msit@berkeley.edu>
2016-11-13 09:14:45 -0800

    submission

--- src/FrameMech.java
+++ src/FrameMech.java
@@ -33,6 +33,7 @@ public class FrameMech extends JFrame {
                 m.drawBefore(SampleMolecule.bromine(), m);
                 m.drawAfter(SampleMolecule.bromine(), new LiF(), m);
                 m.lif.setBackground(Color.green);
+                m.drawDescription("LiF- performs a reaction called SN2 on the molecule where the leaving group (usually F, Cl, Br, or I) leaves and the F- joins the now broken molecule.",m);
                 me.dispose();
             }
         });
@@ -43,6 +44,7 @@ public class FrameMech extends JFrame {
                 m.drawBefore(SampleMolecule.alcohol(), m);
                 m.drawAfter(SampleMolecule.alcohol(), new PCC(), m);
                 m.pcc.setBackground(Color.green);
+                m.drawDescription("PCC- This is a special reagent that reacts with a molecule that changes all alcohol groups (OH groups) into an O double bonded to a carbon.",m);
                 me.dispose();
             }
         });
@@ -53,6 +55,7 @@ public class FrameMech extends JFrame {
                 m.drawBefore(SampleMolecule.bromine(), m);
                 m.drawAfter(SampleMolecule.bromine(), new NaI(), m);
                 m.nai.setBackground(Color.green);
+                m.drawDescription("NaI- Similar to LiF. SN2 is performed on the molecule where the leaving group (usually F, Cl, Br, or I) leaves and the F- joins the now broken molecule.",m);
                 me.dispose();
             }
         });
@@ -63,6 +66,7 @@ public class FrameMech extends JFrame {
                 m.drawBefore(SampleMolecule.ch3oh(), m);
                 m.drawAfter(SampleMolecule.ch3oh(), new CH3OH(), m);
                 m.hoch3.setBackground(Color.green);
+                m.drawDescription("CH3OH- causes Sn1 since weak attacker so leaving group leaves first an then OCH3 group attaches onto place the group left.", m);
                 me.dispose();
             }
         });
@@ -73,6 +77,7 @@ public class FrameMech extends JFrame {
                 m.drawBefore(SampleMolecule.bromine(), m);
                 m.drawAfter(SampleMolecule.bromine(), new Water(), m);
                 m.h2o.setBackground(Color.green);
+                m.drawDescription("Water- causes the leaving group to leave and an alcohol group to take the place of the group that left.", m);
                 me.dispose();
             }
         });
@@ -91,7 +96,7 @@ public class FrameMech extends JFrame {
 
         setLayout(null);
         setVisible(true);
-        setSize(550,400);
+        setSize(550,600);
         setResizable(false);
     }
 
@@ -106,6 +111,21 @@ public class FrameMech extends JFrame {
     private void drawAfter(Molecule m, Reagent r, JFrame f) {
         JPanel container = new MoleculePanel(r.action(m));
         container.setBounds(280,100,240,240);
+        f.add(container);
+        f.setSize(f.getWidth(), f.getHeight()+1);
+        f.setSize(f.getWidth(), f.getHeight()-1);
+    }
+
+    private void drawDescription(String s, JFrame f) {
+        JPanel container = new JPanel();
+        JTextArea a = new JTextArea(s,5, 30);
+        a.setEditable(false);
+        a.setLineWrap(true);
+        a.setWrapStyleWord(true);
+        a.setBackground(null);
+        container.add(a);
+        container.setBounds(30,360,490,300);
+
         f.add(container);
         f.setSize(f.getWidth(), f.getHeight()+1);
         f.setSize(f.getWidth(), f.getHeight()-1);
