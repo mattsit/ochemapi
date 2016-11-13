cbb506c4c30e97413ed88e90e1049a09fa104f0a
Debbie Pao <dpao@berkeley.edu>
2016-11-13 07:37:09 -0800

    changed sample molecule 2

--- src/SampleMolecule.java
+++ src/SampleMolecule.java
@@ -26,7 +26,7 @@ public class SampleMolecule {
         return alcohol;
     }
 
-    public static Molecule bromine() {
+    public static Molecule bromine() { //test naI or LiF or och3- or ch3oh
         Molecule bromine = new Molecule();
         Atom c1 = new Atom(6);
         Atom c2 = new Atom(6);
@@ -52,6 +52,52 @@ public class SampleMolecule {
         bromine.addBond(c2, h5);
         return bromine;
 
+    }
+
+    public static Molecule ch3oh() { //test ch3oh
+        Molecule ans = new Molecule();
+        Atom c1 = new Atom(6);
+        Atom c2 = new Atom(6);
+        Atom c3 = new Atom(6);
+        Atom c4 = new Atom(6);
+        Atom br = new Atom(35);
+        Atom h = new Atom(1);
+        Atom h2 = new Atom(1);
+        Atom h3 = new Atom(1);
+        Atom h4 = new Atom(1);
+        Atom h5 = new Atom(1);
+        Atom h6 = new Atom(1);
+        Atom h7 = new Atom(1);
+        Atom h8 = new Atom(1);
+        Atom h9 = new Atom(1);
+        ans.addAtom(c1);
+        ans.addAtom(c2);
+        ans.addBond(c1, c2);
+        ans.addAtom(c3);
+        ans.addAtom(c4);
+        ans.addBond(c2, c3);
+        ans.addBond(c3, c4);
+        ans.addAtom(br);
+        ans.addBond(c3, br);
+        ans.addAtom(h);
+        ans.addAtom(h2);
+        ans.addAtom(h3);
+        ans.addBond(h, c1);
+        ans.addBond(h2, c1);
+        ans.addBond(h3, c1);
+        ans.addAtom(h4);
+        ans.addAtom(h5);
+        ans.addBond(c2, h4);
+        ans.addBond(c2, h5);
+        ans.addAtom(h6);
+        ans.addBond(h6, c3);
+        ans.addAtom(h7);
+        ans.addAtom(h8);
+        ans.addAtom(h9);
+        ans.addBond(c4, h7);
+        ans.addBond(c4, h8);
+        ans.addBond(c4, h9);
+        return ans;
     }
 
 }
