3e59b640c800bcad094407431096f73617ec72b9
Debbie Pao <dpao@berkeley.edu>
2016-11-13 07:25:38 -0800

    changed sample molecule

--- src/SampleMolecule.java
+++ src/SampleMolecule.java
@@ -25,4 +25,33 @@ public class SampleMolecule {
 
         return alcohol;
     }
+
+    public static Molecule bromine() {
+        Molecule bromine = new Molecule();
+        Atom c1 = new Atom(6);
+        Atom c2 = new Atom(6);
+        Atom br = new Atom(35);
+        Atom h = new Atom(1);
+        Atom h2 = new Atom(1);
+        Atom h3 = new Atom(1);
+        Atom h4 = new Atom(1);
+        Atom h5 = new Atom(1);
+        bromine.addAtom(c1);
+        bromine.addAtom(c2);
+        bromine.addBond(c1, c2);
+        bromine.addBond(c1, br);
+        bromine.addAtom(h);
+        bromine.addAtom(h2);
+        bromine.addAtom(h3);
+        bromine.addAtom(h4);
+        bromine.addAtom(h5);
+        bromine.addBond(c1, h);
+        bromine.addBond(c1, h2);
+        bromine.addBond(c2, h3);
+        bromine.addBond(c2, h4);
+        bromine.addBond(c2, h5);
+        return bromine;
+
+    }
+
 }
