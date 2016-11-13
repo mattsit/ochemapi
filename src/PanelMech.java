d1fd5199200e847ec5d441e987a52eac14e77d82
Debbie Pao <dpao@berkeley.edu>
2016-11-13 05:40:21 -0800

    changed PanelMech

--- src/PanelMech.java
+++ src/PanelMech.java
@@ -10,14 +10,27 @@ public class PanelMech extends JPanel {
     public void paintComponent(Graphics g) {
         g.drawString("Mech", 20, 20);
 
-        JButton h = new JButton("H");
-        JButton he = new JButton("He");
-        JButton li = new JButton("Li");
-        JButton be = new JButton ("Be");
-        JButton b = new JButton("B");
-        JButton c = new JButton("C");
-        JButton n = new JButton("N");
-        JButton o = new JButton("O");
+        JLabel title = new JLabel("Mechanisms");
+        title.setFont(new Font("Verdana",1,14));
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        title.setBorder(BorderFactory.createLineBorder(new Color(59, 59, 59), 2));
+
+        JButton btnGenerate = new JButton("React!");
+        btnGenerate.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton lif = new JButton("LiF");
+        JButton ch3oh = new JButton("CH3OH");
+        JButton nai = new JButton("NaI");
+        JButton och3 = new JButton("OCH3");
+        JButton h2O = new JButton("H2O");
+
+        JPanel me = this;
+        
+
+
+
+
+
 
     }
 }
