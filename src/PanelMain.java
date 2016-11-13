079a926e37432f7ded753cee7b650e5d5acc0337
Debbie Pao <dpao@berkeley.edu>
2016-11-13 04:09:21 -0800

    changed PanelMain

--- src/PanelMain.java
+++ src/PanelMain.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 /**
  * Created by Matthew on 11/13/2016.
@@ -9,5 +12,17 @@ public class PanelMain extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         g.drawString("Main", 20, 20);
+        g.fill3DRect(0, 0, 20, 20, false);
+        JButton entr = new JButton("Enter");
+        JButton ext = new JButton("Exit");
+
+        ext.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("exiting");
+
+            }
+        });
+        this.add(ext);
     }
+
 }
